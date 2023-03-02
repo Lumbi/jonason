45da2860eae5e1da8852495f0410a449d3c80bbd
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-02 13:50:58 -0500

    Initial Commit

---
+++ jonason/jonason.h
@@ -0,0 +1,18 @@
+//
+//  jonason.h
+//  jonason
+//
+//  Created by Gabriel Lumbi on 2023-03-02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for jonason.
+FOUNDATION_EXPORT double jonasonVersionNumber;
+
+//! Project version string for jonason.
+FOUNDATION_EXPORT const unsigned char jonasonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <jonason/PublicHeader.h>
+
+
