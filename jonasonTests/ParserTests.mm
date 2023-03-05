fac9e524dfbed5997b2e1ff45ab7f6d48baa927e
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-05 13:51:02 -0500

    Test basic JSON object parsing

---
+++ jonasonTests/ParserTests.mm
@@ -0,0 +1,71 @@
+//
+//  ParserTests.m
+//  jonasonTests
+//
+//  Created by Gabriel Lumbi on 2023-03-03.
+//
+
+#import <XCTest/XCTest.h>
+
+#include <jonason/jonason.h>
+#include <string>
+
+@interface ParserTests : XCTestCase
+
+@end
+
+@implementation ParserTests
+
+- (void) test_object_empty {
+    std::string string(R"( {  } )");
+    jonason::JSONValue json;
+    jonason::parse(string, json);
+
+    XCTAssertEqual(json.tag, jonason::JSONValue::OBJECT);
+    XCTAssertEqual(json["hello"].tag, jonason::JSONValue::JSON_NULL);
+}
+
+- (void) test_object_with_single_string {
+    std::string string(R"( { "hello": "world" } )");
+    jonason::JSONValue json;
+    jonason::parse(string, json);
+
+    XCTAssertEqual(json.tag, jonason::JSONValue::OBJECT);
+    XCTAssertEqual(json["hello"].tag, jonason::JSONValue::STRING);
+    XCTAssertEqual(json["hello"].string, std::string("world"));
+}
+
+- (void) testUnexpectedEOFError {
+    try {
+        std::string string(R"( { "hello" : "world" )");
+        jonason::JSONValue json;
+        jonason::parse(string, json);
+        XCTFail();
+    } catch (const jonason::ParseError& error) {
+        XCTAssertEqual(error.code, jonason::ParseError::UNEXPECTED_EOF);
+    }
+}
+
+- (void) testUnexpectedTokenError {
+    try {
+        std::string string(R"( { "hello" ] "world" } )");
+        jonason::JSONValue json;
+        jonason::parse(string, json);
+        XCTFail();
+    } catch (const jonason::ParseError& error) {
+        XCTAssertEqual(error.code, jonason::ParseError::UNEXPECTED_TOKEN);
+    }
+}
+
+- (void) testUnexpectedLiteralError {
+    try {
+        std::string string(R"( { hello: "world" } )");
+        jonason::JSONValue json;
+        jonason::parse(string, json);
+        XCTFail()
+    } catch (const jonason::ParseError& error) {
+        XCTAssertEqual(error.code, jonason::ParseError::UNEXPECTED_TOKEN);
+    }
+}
+
+@end
