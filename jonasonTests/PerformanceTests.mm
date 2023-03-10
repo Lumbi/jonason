2bff57299c39620d794cc6a321b7b6da4a252133
Gabriel Lumbi <Lumbi@users.noreply.github.com>
2023-03-09 20:59:00 -0500

    Add more performance tests

--- jonasonTests/PerformanceTests.mm
+++ jonasonTests/PerformanceTests.mm
@@ -18,9 +18,9 @@
 
 @implementation PerformanceTests
 
+// canada.json
 
-- (void) testPeformanceBenchmark {
-
+- (void) testPeformanceBenchmark1 {
     NSString* file_path = [[NSBundle bundleForClass:[self class]] pathForResource:@"canada" ofType:@"json"];
 
     [self measureMetrics:[self class].defaultPerformanceMetrics automaticallyStartMeasuring:NO forBlock:^{
@@ -38,7 +38,7 @@
     }];
 }
 
-- (void) testParsePerformance {
+- (void) testParsePerformance1 {
     const char* file_path = [[[NSBundle bundleForClass:[self class]] pathForResource:@"canada" ofType:@"json"] UTF8String];
 
     [self measureMetrics:[self class].defaultPerformanceMetrics automaticallyStartMeasuring:NO forBlock:^{
@@ -55,8 +55,62 @@
     }];
 }
 
-- (void) testTokenizePerformance {
+- (void) testTokenizePerformance1 {
     const char* file_path = [[[NSBundle bundleForClass:[self class]] pathForResource:@"canada" ofType:@"json"] UTF8String];
+
+    [self measureMetrics:[self class].defaultPerformanceMetrics automaticallyStartMeasuring:NO forBlock:^{
+        std::ifstream ifstream;
+        ifstream.open(file_path, std::ifstream::in);
+
+        jonason::Tokenizer tokenizer(ifstream);
+        [self startMeasuring];
+        while (true) {
+            jonason::Token token = tokenizer.get_next();
+            if (token.is_eof()) break;
+        }
+        [self stopMeasuring];
+    }];
+}
+
+// citm_catalog.json
+
+- (void) testPeformanceBenchmark2 {
+    NSString* file_path = [[NSBundle bundleForClass:[self class]] pathForResource:@"citm_catalog" ofType:@"json"];
+
+    [self measureMetrics:[self class].defaultPerformanceMetrics automaticallyStartMeasuring:NO forBlock:^{
+        NSInputStream* istream = [[NSInputStream alloc] initWithFileAtPath: file_path];
+        [istream open];
+        NSError* error = nil;
+
+        [self startMeasuring];
+        [NSJSONSerialization JSONObjectWithStream:istream options:0 error: &error];
+        [self stopMeasuring];
+
+        if (error) { XCTFail(); }
+
+        [istream close];
+    }];
+}
+
+- (void) testParsePerformance2 {
+    const char* file_path = [[[NSBundle bundleForClass:[self class]] pathForResource:@"citm_catalog" ofType:@"json"] UTF8String];
+
+    [self measureMetrics:[self class].defaultPerformanceMetrics automaticallyStartMeasuring:NO forBlock:^{
+        std::ifstream ifstream;
+        ifstream.open(file_path, std::ifstream::in);
+
+        [self startMeasuring];
+        try {
+            jonason::parse(ifstream);
+        } catch (...) {
+            XCTFail();
+        }
+        [self stopMeasuring];
+    }];
+}
+
+- (void) testTokenizePerformance2 {
+    const char* file_path = [[[NSBundle bundleForClass:[self class]] pathForResource:@"citm_catalog" ofType:@"json"] UTF8String];
 
     [self measureMetrics:[self class].defaultPerformanceMetrics automaticallyStartMeasuring:NO forBlock:^{
         std::ifstream ifstream;
